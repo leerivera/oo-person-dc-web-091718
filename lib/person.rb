aca87547001a8fd28a81daa1d93e521ab1314e25
tomgiagtz <tom.giagtzoglou@gmail.com>
2018-09-19 16:16:43 -0400

    add work_out, call-friend, start_conversation methods

--- lib/person.rb
+++ lib/person.rb
@@ -22,15 +22,10 @@ class Person
       @hygiene = 0
     end
   end
-  
-
 
   def clean?
     @hygiene > 7
-      
-    
-  end
-  
+  end 
 
   def happiness=(points)
     @happiness = points
@@ -41,7 +36,6 @@ class Person
       @happiness = 0
     end
   end
-  
 
   def happy?
     @happiness > 7
@@ -55,20 +49,40 @@ class Person
   end
   
   def take_bath
-
     self.hygiene += 4
     return "♪ Rub-a-dub just relaxing in the tub ♫"
   end
 
   
   def work_out
-    
+    self.happiness += 2
+    self.hygiene -= 3
+
+    return "♪ another one bites the dust ♫"
   end
-  
-  def call_friend
+  # passing in an instance of Person, named friend
+  def call_friend(friend)
+    #add 3 to this person's happiness (self)
+    self.happiness += 3
+    #add 3 to the friend's happiness
+    friend.happiness += 3
+
+    #return phone call string
+    "Hi #{friend.name}! It's #{self.name}. How are you?"
   end 
   
-  def start_conversation
+  def start_conversation(person, topic)
+    if topic == "politics"
+      self.happiness -= 2
+      person.happiness -= 2
+      "blah blah partisan blah lobbyist"
+    elsif topic == "weather"
+      self.happiness += 1
+      person.happiness += 1
+      "blah blah sun blah rain"
+    else
+      "blah blah blah blah blah"
+    end
   end 
   
 end
