c84346db2b57b3a86bef93a7c75495c3150d9dff
Nick Rivera <leerivera80@gmail.com>
2018-09-19 19:54:11 +0000

    person oo

--- lib/person.rb
+++ lib/person.rb
@@ -1,1 +1,41 @@
 # your code goes here
+class Person
+  attr_reader :name
+  attr_accessor :bank_account, :happiness, :hygiene
+  def initialize(name)
+    @name = name
+    @bank_account = 25
+    @happiness = 8
+    @hygiene = 8
+  end
+  def clean?
+    @hygiene > 7
+      
+    
+  end
+  
+  def happy?
+    @happiness > 7
+    
+  end
+  
+  def get_paid(amount)
+    @bank_account += amount
+    return "all about the benjamins"
+    
+  end
+  
+  def take_bath
+    @hygiene += 4
+  end
+
+  def work_out
+  end
+  
+  def call_friend
+  end 
+  
+  def start_conversation
+  end 
+  
+end
